bff9ef980d582650bbe60b19763c6096f803a38a
Yao Yu <yao97ming@gmail.com>
2020-03-18 15:36:15 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Yao Yu on 3/18/20.
+//  Copyright © 2020 Yao Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
